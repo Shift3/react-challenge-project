506487d4af0b87bf7f0d8ac8bcb239d6a42cbfd7
Liz Langois <lizl714@yahoo.com>
2020-08-26 10:53:22 -0700

    fix(server ip): allow Win10/Docker Desktop for Windows to use localhost for Server IP
    Add check on Win10 using appVersion. If found, clear windowsPlatforms to
    allow Win10 to use localhost.

--- application/src/private.js
+++ application/src/private.js
@@ -1,3 +1,12 @@
-const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+var windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+
+// Win 10/Docker Desktop for Windows uses localhost
+// Win10 has a platform of Win32 - so need an additional check
+// on the appVersion to avoid matching the platform
+const win10 = RegExp("Windows NT 10.0*");
+
+if (win10.test(window.navigator.appVersion)) {
+    windowsPlatforms = [];
+}
 
 export const SERVER_IP = windowsPlatforms.indexOf(window.navigator.platform) < 0 ? 'http://localhost:4000' : 'http://192.168.99.100:4000';
