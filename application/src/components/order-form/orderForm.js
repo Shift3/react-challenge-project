f065d0de9721042228481b6041670a4da20b91b1
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-06 01:32:30 -0800

    add edit and delete api calls

--- application/src/components/order-form/orderForm.js
+++ application/src/components/order-form/orderForm.js
@@ -13,19 +13,20 @@ const mapStateToProps = (state) => ({
 class OrderForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            order_item: "",
-            quantity: "1"
-        }
-    }
+      this.state = {
+              order_item: "",
+              quantity: "1"
+      }
+      }
+  
 
-    menuItemChosen(event) {
-        this.setState({ item: event.target.value });
+      menuItemChosen(event) {
+        this.setState({ order_item: event.target.value });
     }
 
     menuQuantityChosen(event) {
         this.setState({ quantity: event.target.value });
-    }
+      }
 
     submitOrder(event) {
         event.preventDefault();
@@ -47,37 +48,38 @@ class OrderForm extends Component {
     }
 
     render() {
-        return (
-            <Template>
-                <div className="form-wrapper">
-                    <form>
-                        <label className="form-label">I'd like to order...</label><br />
-                        <select 
-                            value={this.state.order_item} 
-                            onChange={(event) => this.menuItemChosen(event)}
-                            className="menu-select"
-                        >
-                            <option value="" defaultValue disabled hidden>Lunch menu</option>
-                            <option value="Soup of the Day">Soup of the Day</option>
-                            <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
-                            <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
-                            <option value="Chili Con Carne">Chili Con Carne</option>
-                        </select><br />
-                        <label className="qty-label">Qty:</label>
-                        <select value={this.state.quantity} onChange={(event) => this.menuQuantityChosen(event)}>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                        </select>
-                        <button type="button" className="order-btn" onClick={(event) => this.submitOrder(event)}>Order It!</button>
-                    </form>
-                </div>
-            </Template>
-        );
-    }
+		const { order_item, quantity, _id, isEdit, menuQuantityChosen, menuItemChosen, submitOrder } = this.props;
+    return (
+      <Template>
+        <div className="form-wrapper">
+          <form>
+            <label className="form-label">I'd like to order...</label><br />
+            <select 
+              value={isEdit ? order_item : this.state.order_item} 
+              onChange={isEdit ? menuItemChosen : (event) => this.menuItemChosen(event)}
+              className="menu-select"
+            >
+              <option value="" defaultValue disabled hidden>Lunch menu</option>
+              <option value="Soup of the Day">Soup of the Day</option>
+              <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
+              <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
+              <option value="Chili Con Carne">Chili Con Carne</option>
+            </select><br />
+            <label className="qty-label">Qty:</label>
+            <select value={isEdit ? quantity : this.state.quantity} onChange={isEdit ? menuQuantityChosen : (event) => this.menuQuantityChosen(event)}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+							<option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+            <button type="button" className="order-btn" value={_id ? _id : ''} onClick={isEdit ? submitOrder : (event) => this.submitOrder(event)}>Order It!</button>
+          </form>
+        </div>
+      </Template>
+    );
+  }
 }
 
 export default connect(mapStateToProps, null)(OrderForm);
