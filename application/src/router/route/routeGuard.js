816862ab8270884663b5af47665152f5e592689d
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-09 00:31:50 -0800

    require login for order-form

---
+++ application/src/router/route/routeGuard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+})
+
+const AuthRoute = ({ component: Component, auth, ...rest }) => {
+    return (
+        <Route { ...rest }>
+            { auth.token 
+                ? <Component />
+                : <Redirect to={"/login"} />
+            }
+        </Route>
+    )
+}
+
+export default connect(mapStateToProps, null)(AuthRoute); 
