e88957a1448810a5478ac6b913f8bd07cccfee22
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-09 02:19:10 -0800

    add protected route to redirect to login page

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch, Redirect } from 'react-router-dom';
 import { Main, Login, OrderForm, ViewOrders } from '../components';
-import routeGuard from './route/routeGuard'
+import { connect } from 'react-redux';
+
+
+const protectedRoute = ({ component: Component, auth, ...rest }) => (
+  <Route{...rest} render={props => (
+    !!auth.email && !!auth.token
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+);
+const mapStateToProps = (state) => ({ auth: state.auth });
+const PrivateRoute = connect(mapStateToProps)(protectedRoute)
+
 const AppRouter = (props) => {
   return (
     <Router>
