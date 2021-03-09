2633c6a4adb43c3ce0695aef914f48b21e2142dd
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-09 02:11:37 -0800

    loginUser action

--- application/src/components/login/login-form/loginForm.js
+++ application/src/components/login/login-form/loginForm.js
@@ -5,6 +5,7 @@ import { loginUser } from '../../../redux/actions/authActions'
 const mapActionsToProps = dispatch => ({
   commenceLogin(email, password) {
     dispatch(loginUser(email, password))
+    return dispatch(loginUser(email, password))
   }
 })
 const mapStateToProps = (state) => ({
@@ -23,9 +24,9 @@ class LoginForm extends Component {
     }
   }
 
-  login(e) {
+  async login(e) {
     e.preventDefault();
-    this.props.commenceLogin(this.state.email, this.state.password);
+     await this.props.commenceLogin(this.state.email, this.state.password);
     this.props.onLogin();
   }
 
