768e9d1a227b1ed6c31406855365efbda8a1222c
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2021-03-14 20:59:29 -0700

    feat(viewOrders): delete order feature implemented

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
 import './viewOrders.css';
 
+
+const DELETE_ORDER_URL = `${SERVER_IP}/api/delete-order`
+
 class ViewOrders extends Component {
     state = {
-        orders: []
+        orders: [],
+        editInfo: null
     }
 
+
     componentDidMount() {
+        this.getCurrentOrders();
+      }
+    
+      getCurrentOrders() {
         fetch(`${SERVER_IP}/api/current-orders`)
-            .then(response => response.json())
-            .then(response => {
-                if(response.success) {
-                    this.setState({ orders: response.orders });
-                } else {
-                    console.log('Error getting orders');
-                }
-            });
+          .then((response) => response.json())
+          .then((response) => {
+            if (response.success) {
+              this.setState({ orders: response.orders });
+            } else {
+              console.log('Error getting orders');
+            }
+          })
+          .catch((err) => console.error(err));
+      }
+
+    async deleteOrder(orderID) {
+        let order_id = orderID
+        await fetch(DELETE_ORDER_URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                id: order_id
+            }),
+            headers: {
+                'Content-type': 'application/json',
+            }
+
+        })
+        .then((res) => {
+            console.log(res)
+            this.setState(this.getCurrentOrders())
+        })
+        .catch((err) => console.error(err));
     }
 
     render() {
-        return (
+        return (            
             <Template>
                 <div className="container-fluid">
                     {this.state.orders.map(order => {
@@ -37,8 +67,8 @@ class ViewOrders extends Component {
                                     <p>Quantity: {order.quantity}</p>
                                  </div>
                                  <div className="col-md-4 view-order-right-col">
-                                     <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
+                                     <button className="btn btn-success" >Edit</button>
+                                     <button className="btn btn-danger" onClick={this.deleteOrder.bind(this, order._id)}>Delete</button>
                                  </div>
                             </div>
                         );
